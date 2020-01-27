c2f93ce180fe3f97a5dfe15c9558bf4bf5f57983
Tanwir Lalani <tlalani@hawk.iit.edu>
2020-01-27 00:11:28 -0600

    added envirnoment checks

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBHfOgO7deLVgQNaLd-Om2YMBXGkNooVMc",
-    authDomain: "attendance-rec.firebaseapp.com",
-    databaseURL: "https://attendance-rec.firebaseio.com",
-    projectId: "attendance-rec",
-    storageBucket: "attendance-rec.appspot.com",
-    messagingSenderId: "69537583971"
+    apiKey: "AIzaSyD1_Ll6I9w6HNert4l4Fr9B8rdOrBJc0Lg",
+    authDomain: "rec-attendance-dev.firebaseapp.com",
+    databaseURL: "https://rec-attendance-dev.firebaseio.com",
+    projectId: "rec-attendance-dev",
+    storageBucket: "rec-attendance-dev.appspot.com",
+    messagingSenderId: "649719202377",
+    appId: "1:649719202377:web:b777395c4fd73d67bbf598",
+    measurementId: "G-W69BC1WBC4"
   }
 };
